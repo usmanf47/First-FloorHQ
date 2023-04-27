6fa5d08f26a2b661e6b0ecca39a6456602b6c62e
unknown <you@example.com>
2023-04-27 10:34:05 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room",
+      "name": "Living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1688,
+      "initialViewParameters": {
+        "yaw": 1.5795137756266033,
+        "pitch": 0.1906498299632311,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1473303067511793,
+          "pitch": 0.0939503085184068,
+          "rotation": 0.7853981633974483,
+          "target": "2-bedroom-1"
+        },
+        {
+          "yaw": 0.45998129280395084,
+          "pitch": 0.049064053838566934,
+          "rotation": 0.7853981633974483,
+          "target": "1-bedroom-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bedroom-2",
+      "name": "Bedroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -3.0807323431223903,
+        "pitch": -0.21413544448107658,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5975408567168898,
+          "pitch": 0.02764207094170068,
+          "rotation": 0.7853981633974483,
+          "target": "0-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom-1",
+      "name": "Bedroom 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1688,
+      "initialViewParameters": {
+        "yaw": 1.1149996163172986,
+        "pitch": 0.014792387077218194,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3336365732696276,
+          "pitch": 0.046547704017466884,
+          "rotation": 0,
+          "target": "0-living-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "First Floor 4K 360 Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
